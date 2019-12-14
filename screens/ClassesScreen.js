9a0b53729d3a58fd07374cd20814db889b6941bf
hishamabonjim <hisham.abonjim@gmail.com>
2019-12-14 15:44:34 +0300

    +pdg

--- screens/ClassesScreen.js
+++ screens/ClassesScreen.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { ScrollView, StyleSheet ,Button , Text} from 'react-native';
+import { ScrollView, View ,  StyleSheet ,Button , Text} from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
 
 export default class ClassesScereen extends React.Component  {
   
+
+
     render() {
           return (
     <ScrollView style={styles.container}>
@@ -11,8 +13,22 @@ export default class ClassesScereen extends React.Component  {
        * Go ahead and delete ExpoLinksView and replace it with your content;
        * we just wanted to provide you with some helpful links.
        */}
-      <Button  title=" الصف الخامس"   onPress={()=>{this.props.navigation.navigate("Main")}} /> 
-      
+       
+       <View style={styles.btn}> 
+            <Button style={styles.btn}  title=" الصف الخامس"   onPress={()=>{this.props.navigation.navigate("Home" , {class :'5'} )}} /> 
+      </View>
+      <View style={styles.btn}>
+      <Button style={styles.btn}  title=" الصف السادس"   onPress={()=>{this.props.navigation.navigate("Home" , {class :'6'})}} />
+</View>
+<View style={styles.btn}>
+      <Button style={styles.btn}  title=" الصف السابع"   onPress={()=>{this.props.navigation.navigate("Home"  ,{class :'7'} ) }} />
+      </View>
+      <View style={styles.btn}>
+      <Button style={styles.btn}  title=" الصف الثامن"   onPress={()=>{this.props.navigation.navigate("Home", {class :'8'})}} />
+      </View>
+      <View style={styles.btn}>
+      <Button   title=" الصف التاسع"   onPress={()=>{this.props.navigation.navigate("Home" , {class :'9'})}} />
+      </View> 
     </ScrollView>
   );
     }
@@ -26,6 +42,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 50,
-    backgroundColor: '#fff',
-  },
+    backgroundColor: '#fff', 
+    
+  }, 
+  btn:{ 
+    paddingTop: 25,
+    flex: 1,
+    }
 });
